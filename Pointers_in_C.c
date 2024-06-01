14e4e8ff0b155786b33b030b68d1b19c61fa814b
Sameerdev512 <156842673+Sameerdev512@users.noreply.github.com>
2024-06-01 22:09:58 +0530

    Create Pointers_in_C.c

---
+++ Pointers_in_C.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+void update(int *a,int *b) {
+    //Complete this function  
+    int i , j; 
+    i = *a + *b; 
+    j = *a-*b;
+    *a = i;
+    if(j<0)
+    {
+        *b = -2*j+j;
+    }
+    else{
+    *b = j;
+    } 
+}
+
+int main() {
+    int a, b;
+    
+    int *pa = &a;
+    int *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+    
+    return 0;
+}
